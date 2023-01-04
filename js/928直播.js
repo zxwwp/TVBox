650842eb5121c5bd2978e61a02bc0237eb1cd8aa
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-01-04 03:11:51 +0000

    update js/928直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/928直播.js
+++ js/928直播.js
@@ -1,6 +1,6 @@
 var rule = {
     title:'928直播',
-    host:'http://www.928.tv/',
+    host:'http://www.928.tv',
     url:'/?s=0&t=1&a=fyclass&g=fypage',
     searchUrl:'',
     searchable:0,
@@ -11,8 +11,7 @@ var rule = {
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    play_parse:true,
-    //play_parse:false,
+    play_parse:false,
     lazy:'',
     limit:6,
     double:false,
