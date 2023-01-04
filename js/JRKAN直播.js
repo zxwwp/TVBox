c8f5963f438f6780a605e73cb76546e6a65d75a1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-01-04 01:26:47 +0000

    update js/JRKAN直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/JRKAN直播.js
+++ js/JRKAN直播.js
@@ -11,7 +11,7 @@
 
 var rule = {
     title:'JRKAN直播',
-    host:'http://www.jrkankan.com/?lan=1',
+    host:'http://www.jrskan.com/?lan=1',
 	// JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com
 	// JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
