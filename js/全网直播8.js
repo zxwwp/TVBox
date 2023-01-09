df28c24e66051dd8c88a51f45d0514d4ce4373cf
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-01-09 08:07:09 +0000

    update js/全网直播8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/全网直播8.js
+++ js/全网直播8.js
@@ -1,12 +1,12 @@
 var rule = {
     title:'全网直播8',
-    host:'https://www.zhibo8.net/match-list.html',
+    host:'https://www.zhibo8.net',
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
     class_name:'全部',
-    class_url:'tab-all active',
+    class_url:'/match-list.html',
     headers:{
         'User-Agent':'PC_UA'
     },
@@ -16,7 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-   一级:'.todayMatch .contenTab;.myList div:gt(0)&&Text;img&&src;.status&&Text;a&&href',
+   一级:'.match-list li;.myList div:gt(0)&&Text;img&&src;.status&&Text;a&&href',
     二级:{title:'.msg ul&&Text;.msg ul&&Text',img:'img:eq(2)&&src',desc:';;;.nameOne&&Text;.nameTwo&&Text',content:'._t&&Text',tabs:'.checkChannel li:eq(0)',tab_text:'p&&Text',lists:'.checkChannel li:gt(0)',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
