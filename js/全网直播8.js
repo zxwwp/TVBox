8e51bea1862c638c80dd276e11150ba1756dee52
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-01-11 08:21:34 +0000

    update js/全网直播8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/全网直播8.js
+++ js/全网直播8.js
@@ -1,13 +1,18 @@
 var rule = {
     title:'全网直播8',
     host:'https://www.zhibo8.net',
-    url:'/match/fyclass/live',
+     url:'/?s=0&t=1&a=fyclass&g=fypage',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
+    //class_name:'热门&足球&篮球',
+    //class_url:'0&1&2',        
+    //class_name:'热门',
+    //class_url:'0',
+
     class_parse:'.nav li;a&&Text;a&&href;/live_/(\\d+)/html?project=/(\\d+)/&game=/(\\d+)',
     headers:{
-        'User-Agent':'PC_UA'
+'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
     play_parse:true,
