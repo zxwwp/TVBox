ec2ee98f16924be3e938ab072340dac0536c38a5
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-02 07:03:59 +0000

    update js/全网直播8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/全网直播8.js
+++ js/全网直播8.js
@@ -1,28 +1,58 @@
 var rule = {
-    title:'全网直播8',
-    host:'https://www.zhibo8.net',
-     url:'/?s=0&t=1&a=fyclass&g=fypage',
-    searchUrl:'',
-    searchable:0,
-    quickSearch:0,
-    //class_name:'热门&足球&篮球',
-    //class_url:'0&1&2',        
-    class_name:'全网直播8',
-    class_url:'0',
+    title:'全网直播8',//规则标题,没有实际作用,但是可以作为cms类名称依据
+    host:'https://www.zhibo8.net',//规则标题,没有实际作用,但是可以作为cms类名称依据
+     url:'/?s=0&t=1&a=fyclass&g=fypage',//网站的分类页面链接
+    searchUrl:'',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
+    searchable:0,//是否启用全局搜索,
+    quickSearch:0,//是否启用快速搜索,
+    filterable:0,//是否启用筛选,
     headers:{
-'User-Agent':'MOBILE_UA'
-    },
-    timeout:5000,
+        'User-Agent':'MOBILE_UA',
+        "Cookie": "searchneed=ok"
+    },//网站的请求头,完整支持所有的,常带ua和cookies
+timeout:5000,//网站的全局请求超时,默认是3000毫秒
+    class_name:'篮球直播&足球直播',//静态分类名称拼接
+    class_url:'1&2',//静态分类标识拼接
+    //动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
+class_parse:'#side-menu:lt(1) li;a&&Text;a&&href;com/(.*?)/',
+    // 除开全局过滤之外还需要过滤哪些标题不视为分类
+    cate_exclude:'',
+    // 除开全局动态线路名过滤之外还需要过滤哪些线路名标题不视为线路
+    tab_exclude:'',
+    // 服务器解析播放
     play_parse:true,
+    // play_json　传数组或者　类　true/false 比如 0,1 如果不传会内部默认处理 不传和传0可能效果不同
+    // 效果等同说明: play_json:[{re:'*', json:{jx:0, parse:1}}], 等同于 play_json:0,
+    play_json:[{
+        re:'*',
+        json:{
+            jx:1,
+            parse:1,
+        },
+    }],
+    //控制不同分类栏目下的总页面,不填就是默认999.哔哩影视大部分分类无法翻页，都需要指定页数为 1
+    pagecount:{"1":1,"2":1,"3":1,"4":1,"5":1,"7":1,"时间表":1},
+    // 自定义免嗅 
     lazy:'',
+    // 首页推荐显示数量
     limit:6,
-    double:false,
-    推荐:'*',
-    一级:'.main-left .main-itme;.col-home;.btn&&Text;a&&href',
-    二级:{
-	    "title":".play-info-soccer&&Text",
-	    "desc":";;;div.col-home:eq(0)&&Text;div.col-home:eq(1)&&Text",
-	    "content":"div.col-time&&Text",
-	},
-    搜索:'',
+    double:true,//是否双层列表定位,默认false
+    // 对图片加了referer验证的有效,海阔专用,普通规则请勿填写此键值
+    图片来源:'@Referer=http://www.jianpianapp.com@User-Agent=jianpian-version350',
+    
+    // js写法，仅js模式1有效.可以用于代码动态获取全局cookie之类的
+    // 可操作变量有 rule_fetch_params,rule,以及基础的网页访问request,post等操作
+    预处理:'rule_fetch_params.headers.Cookie = "xxxx";',
+    // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
+    推荐:'.col-sm-6;h3&&Text;img&&data-src;.date&&Text;a&&href',
+    // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    一级:'.col-sm-6;h3&&Text;img&&data-src;.date&&Text;a&&href',
+    // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
+    // 二级 title: 片名;类型
+    // 二级 desc: 主要信息;年代;地区;演员;导演
+    // 或者 {title:'',img:'',desc:'',content:'',tabs:'',lists:'',tab_text:'body&&Text',list_text:'body&&Text',list_url:'a&&href'} 同海阔dr二级
+    二级:'*',
+    // 搜索可以是*,集成一级，或者跟一级一样的写法 列表;标题;图片;描述;链接;详情
+    搜索:'*',
 }
