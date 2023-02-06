7ee7c759a00ec111145ac83e75e2b9914ba3813d
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-06 03:26:16 +0000

    update js/310直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/310直播.js
+++ js/310直播.js
@@ -17,7 +17,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;js:tabs.forEach(function(it)var img=pd(it,img&&src))',
+    一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;js:var img=pd(it,img&&src))',
    //一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
     //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:'*', 
