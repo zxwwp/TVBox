b8cb6e067367d8f344f98dc81e063c5f1f1ed944
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-07 06:40:30 +0000

    update 体育接口/全网直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- 体育接口/全网直播吧.js
+++ 体育接口/全网直播吧.js
@@ -14,7 +14,7 @@ var rule = {
     // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
     推荐:'*',
-    一级:'main-left .match-list;.col .flex&&Text;img&&src;.btn&&Text;a&&href',
+    一级:'.main-left.match-list;.col.flex&&Text;img&&src;.btn&&Text;a&&href',
    //一级:'.list-group .group-game-item;.d-none&&Text;img&&src;.btn&&Text;a&&href',// 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
  // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
     // 二级 title: 片名;类型
@@ -28,6 +28,7 @@ var rule = {
 	    "tabs":"js:TABS=['全网直播吧']",
 	    "lists":"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)});"
 	},
+
     // 搜索可以是*,集成一级，或者跟一级一样的写法 列表;标题;图片;描述;链接;详情
     搜索:'',
 }
