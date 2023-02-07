65febc129a08c0e9636df8a6d1ecc16a9fb90a42
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-07 01:56:32 +0000

    update 体育接口/全网直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- 体育接口/全网直播吧.js
+++ 体育接口/全网直播吧.js
@@ -1,21 +1,15 @@
 var rule = {
     title:'全网直播吧',//规则标题,没有实际作用,但是可以作为cms类名称依据
-    //编码:'',//不填就默认utf-8
     host:'https://www.zhibo8.net',//网页的域名根,包含http头如 https://www,baidu.com
     url:'/?s=0&t=1&a=fyclass&g=fypage',//网站的分类页面链接, fyclass是分类标签 fypage是页数
-    //searchUrl:'',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
-    //searchable:0,//是否启用全局搜索
-    //quickSearch:0,//是否启用快速搜索
-    //filterable:0,//是否启用筛选,
     class_name:'首页',//静态分类名称拼接
-    class_url:'0',    //静态分类标识拼接    
+    class_url:'/',    //静态分类标识拼接    
     headers:{'User-Agent':'MOBILE_UA'},//网站的请求头,完整支持所有的,常带ua和cookies
     timeout:5000,//网站的全局请求超时,默认是3000毫秒
     play_parse:false,// 服务器解析播放
     lazy:'', // 自定义免嗅 
-    limit:6,    // 首页推荐显示数量
+    limit:6, // 首页推荐显示数量
     double:false,//是否双层列表定位,默认false
-    //图片来源:'@Referer=https://gitlab.com/duomv/duo/-/raw/main/txt/duotv.png',// 对图片加了referer验证的有效,海阔专用,普通规则请勿填写此键值
     // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
     推荐:'*',
