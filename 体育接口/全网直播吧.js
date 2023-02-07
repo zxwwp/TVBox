20f6f598d9ab990c44485893123b8d82924be17f
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-07 01:29:52 +0000

    update 体育接口/全网直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- 体育接口/全网直播吧.js
+++ 体育接口/全网直播吧.js
@@ -1,14 +1,13 @@
 var rule = {
     title:'全网直播吧',//规则标题,没有实际作用,但是可以作为cms类名称依据
-    编码:'',//不填就默认utf-8
+    //编码:'',//不填就默认utf-8
     host:'https://www.zhibo8.net',//网页的域名根,包含http头如 https://www,baidu.com
     homeUrl:'https://www.zhibo8.net',//网站的首页链接,可以是完整路径或者相对路径,用于分类获取和推荐获取 fyclass是分类标签 fypage是页数
     url:'',//网站的分类页面链接
-    searchUrl:'',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
-    searchable:0,//是否启用全局搜索
-    quickSearch:0,//是否启用快速搜索
-    filterable:0,//是否启用筛选,
-
+    //searchUrl:'',//搜索链接 可以是完整路径或者相对路径,用于分类获取和推荐获取 **代表搜索词 fypage代表页数
+    //searchable:0,//是否启用全局搜索
+    //quickSearch:0,//是否启用快速搜索
+    //filterable:0,//是否启用筛选,
     class_name:'首页',//静态分类名称拼接
     class_url:'nav-item active href',    //静态分类标识拼接    
     headers:{'User-Agent':'MOBILE_UA'},//网站的请求头,完整支持所有的,常带ua和cookies
