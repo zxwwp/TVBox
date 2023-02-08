2142a4660bdc8620d00e2bb2421cee26bf264616
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-08 03:22:14 +0000

    update 体育接口/全网直播8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- 体育接口/全网直播8.js
+++ 体育接口/全网直播8.js
@@ -1,11 +1,10 @@
-muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
+muban.首图2.二级.tabs = '.nav-item li.dpplay&&li';
 var rule = Object.assign(muban.首图2,{
     title:'全网直播8',
     ali_token:'{{ali_token}}',
     bili_cookie:'{{bili_cookie}}',
     host:'https://www.zhibo8.net',
     url:'/live_1.html?project=1&game=0',//网站的分类页面链接, fyclass是分类标签 fypage是页数
-    searchUrl:'/search/**----------fypage---.html',
-    class_parse:'.nav-item li;a&&Text;a&&href',
+    searchUrl:'/**----------fypage---.html',
     图片来源: "https://gitlab.com/duomv/duo/-/raw/main/txt/duotv.png",
 });
