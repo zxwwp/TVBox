aff67d8dc603f0598842e08bf3dced3895adaedb
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-08 01:56:32 +0000

    add js/新视觉影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ js/新视觉影院.js
@@ -0,0 +1,9 @@
+muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
+var rule = Object.assign(muban.首图2,{
+    title:'新视觉影院',
+    ali_token:'{{ali_token}}',
+    bili_cookie:'{{bili_cookie}}',
+    host:'https://www.6080dy2.com/',
+    url:'/vodtype/fyclass-fypage.html',
+    searchUrl:'/search/**----------fypage---.html',
+});
