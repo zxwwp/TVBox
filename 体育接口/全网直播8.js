138147f41d17e383266ef012e1e3de749ab7ca2b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-08 03:18:01 +0000

    rename 体育接口/全网直播8.json to 体育接口/全网直播8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ 体育接口/全网直播8.js
@@ -0,0 +1,10 @@
+muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
+var rule = Object.assign(muban.首图2,{
+    title:'全网直播8',
+    ali_token:'{{ali_token}}',
+    bili_cookie:'{{bili_cookie}}',
+    host:'https://www.zhibo8.net',
+    url:'/channel/fyclass-fypage.html',
+    searchUrl:'/search/**----------fypage---.html',
+    图片来源: "https://gitlab.com/duomv/duo/-/raw/main/txt/duotv.png",
+});
