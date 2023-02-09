6132ee13138bc268b05c7ad6736aca312cc2d664
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-09 07:03:33 +0000

    add tiyu/直播车直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ tiyu/直播车直播.js
@@ -0,0 +1,19 @@
+var rule = {
+    title:'直播车直播',
+    host:'http://www.zhiboche.tv/',
+    url:'/?s=0&t=1&a=fyclass&g=fypage',
+    class_name:'篮球&足球&综合体育',
+    class_url:'2&1&3',         
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    play_parse:false,
+    lazy:'',
+    limit:6,
+    double:false,
+    推荐:'*',
+   一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
+   二级:'*', 
+    搜索:'',
+}
