3841716943b3c0172fe17310d6b62dfe5eaf477b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-09 06:56:53 +0000

    add tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ tiyu/926直播.js
@@ -0,0 +1,19 @@
+var rule = {
+    title:'926直播',
+    host:'http://www.926.tv',
+    url:'/?s=0&t=1&a=fyclass&g=fypage',
+    class_name:'篮球&足球&体育&电竞',
+    class_url:'2&1&3&4',         
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    play_parse:false,
+    lazy:'',
+    limit:6,
+    double:false,
+    推荐:'*',
+   一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
+   二级:'*', 
+    搜索:'',
+}
