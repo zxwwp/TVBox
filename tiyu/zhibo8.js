15a76277ad4d94f4799e29a8bb66da9484173e78
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-10 03:37:13 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -5,7 +5,8 @@ var rule = {
     class_name:'全部',
     class_url:'/',         
     headers:{
-        'User-Agent':'MOBILE_UA'
+        //'User-Agent':'MOBILE_UA'
+        'User-Agent':'PC_UA'
     },
     timeout:5000,
     play_parse:false,
