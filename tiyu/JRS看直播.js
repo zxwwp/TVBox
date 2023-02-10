7a0ead883f5e5bd0b34dca8683dd81d58650496f
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-10 06:56:57 +0000

    update tiyu/JRS看直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRS看直播.js
+++ tiyu/JRS看直播.js
@@ -16,7 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.row a;li div:gt(2):lt(2)&&Text;img&&src;.match-start-time&&Text;a&&href',
+    一级:'.row a;li div:gt(2):lt(6)&&Text;img&&src;.match-start-time&&Text;a&&href',
     二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:'',tab_text:'',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
