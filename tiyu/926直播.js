e0d130aec9b99b28d5612bc5da26dc565b6f1437
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-10 01:57:27 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -13,7 +13,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-   一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.zhudui img&&src;a&&t-nzf-o;a&&href',
+   一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.team zhudui img&&src;a&&t-nzf-o;a&&href',
    二级:'*', 
     搜索:'',
 }
