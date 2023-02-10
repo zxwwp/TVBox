0f6bf019dbaed4350c544aa511b42d64f194e0ad
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-10 23:44:41 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -12,8 +12,8 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.list-group .group-game-item;.team-name:lt(5)&&Text;img&&src;.btn&&Text;a&&href',
-    //一级:'.list-group .group-game-item;.d-none&&Text;img&&src;.btn&&Text;a&&href',
+    一级:'.list-group .group-game-item;.team-name&&Text;img&&src;.btn&&Text;a&&href',
+    一级:'.list-group .group-game-item;.d-none&&Text;img&&src;.btn&&Text;a&&href',
 //justify-content-between align-items-center
     二级:{
 	    "title":".game-info-container&&Text;.customer-navbar-nav li&&Text",
