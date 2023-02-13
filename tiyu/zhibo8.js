9d79bb9cfdeb0ea391a110cc2a931d5c9d586617
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-13 02:45:19 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -14,7 +14,14 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.main.main-left.match-list;div:gt(1):lt(4)&&Text;img&&src;div&&col-time;a&&href',
-    二级:{title:'.match-list div:lt(1)&&Text;.match-list div:eq(0)&&Text',img:'img&&src',desc:';;;.col-home flex&&Text',content:'.match-list li&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
-    搜索:'',
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.d-touch');tabs.forEach(function(it){var pz=pdfh(it,'.col-home flex&&Text');var ps=pdfh(it,'.col-name&&Text'));var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    
+    二级:{
+		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
+		"img":"img&&src",
+		"desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
+		"content":".sub_list ul&&Text",
+		"tabs":"js:TABS=['直播信号源']",
+		"lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
+	},搜索:'',
 }
