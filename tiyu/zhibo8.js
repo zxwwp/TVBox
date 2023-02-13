63d9453c611126a6ec8f6184b1f6fa8db3a17d71
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-13 07:45:21 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -1,12 +1,14 @@
 var rule = {
-    title:'全网直播8',
-    host:'https://www.zhibo8.net/',
+    title:'JRS直播',
+    host:'http://www.jrskan.net',
     url:'/fyclass',
+    searchUrl:'',
+    searchable:0,
+    quickSearch:0,
     class_name:'全部',
-    class_url:'/',         
+    class_url:'/',
     headers:{
         'User-Agent':'MOBILE_UA'
-        //'User-Agent':'PC_UA'
     },
     timeout:5000,
     play_parse:true,
@@ -14,14 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.match-item');tabs.forEach(function(it){var pz=pdfh(it,'.col-home flex&&Text');var ps=pdfh(it,'.col-name&&Text'));var img=pd(it,'img&&src');var timer=pdfh(it,'.col-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
-    
-    二级:{
-		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
-		"img":"img&&src",
-		"desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",
-		"content":".sub_list ul&&Text",
-		"tabs":"js:TABS=['直播信号源']",
-		"lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
-	},搜索:'',
+    一级:'.row a;li div:gt(2):lt(6)&&Text;img&&src;.match-start-time&&Text;a&&href',
+    二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:'',tab_text:'',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
+    搜索:'',
 }
