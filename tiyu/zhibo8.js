4afb27d262c078ab873c488d3a317e0e09571cb1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-13 01:18:27 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -1,12 +1,12 @@
 var rule = {
     title:'全网直播8',
     host:'https://www.zhibo8.net/',
-    url:'/?s=0&t=1&a=fyclass&g=fypage',
+    url:'/fyclass',
     class_name:'全部',
     class_url:'/',         
     headers:{
-        //'User-Agent':'MOBILE_UA'
-        'User-Agent':'PC_UA'
+        'User-Agent':'MOBILE_UA'
+        ,'User-Agent':'PC_UA'
     },
     timeout:5000,
     play_parse:false,
@@ -14,7 +14,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-   一级:'.match-list .match-item;.col-home flex&&span:lt(2)&&Text;img&&src;div&&col-time;a&&href',
-   二级:'*', 
+    一级:'.main-left:eq(2) li;div:gt(1):lt(4)&&Text;img&&src;div&&col-time;a&&href',
+    二级:{title:'.match-list div:lt(1)&&Text;.match-list div:eq(0)&&Text',img:'img&&src',desc:';;;.col-home flex&&Text',content:'.match-list li&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
     搜索:'',
 }
