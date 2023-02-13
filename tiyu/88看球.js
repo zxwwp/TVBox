6d8d6bc471c55ee4fb5a3b8c6731fa953941470d
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-13 03:06:03 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -12,7 +12,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.list-group-item');tabs.forEach(function(it){var pz=pdfh(it,'.text-right&&Text');var ps=pdfh(it,'.game-type&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.category-game-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.list-group-item');tabs.forEach(function(it){var pz=pdfh(it,'.text-right&&Text');var ps=pdfh(it,'.game-type&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.category-game-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     //一级:'.list-group .group-game-item;.team-name&&Text;img&&src;.btn&&Text;a&&href',
     二级:{
 	    "title":".game-info-container&&Text;.customer-navbar-nav li&&Text",
