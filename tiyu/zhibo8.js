c403ff967ef60806f6c4670bbb614ecccb08799b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-13 08:00:26 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -1,6 +1,6 @@
 var rule = {
     title:'JRS直播',
-    host:'http://www.jrskan.net',
+    host:'https://www.zhibo8.net',
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
@@ -16,7 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.row a;li div:gt(2):lt(6)&&Text;img&&src;.match-start-time&&Text;a&&href',
+    一级:'.main-left li;li div:gt(1):lt(3)&&Text;img&&src;.col&&Text;a&&href',
     二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:'',tab_text:'',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
