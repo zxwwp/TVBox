247c9366ed00beda25a0ede05959b2751facceda
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-14 01:19:34 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -14,8 +14,8 @@ var rule = {
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.group-game-item');tabs.forEach(function(it){var pz=pdfh(it,'.text-right&&Text');var ps=pdfh(it,'.game-type&&Text');var pk=pdfh(it,'.text-left&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.category-game-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
-        //"title":".game-type&&Text;.text-right&&Text,.text-left&&Text",
-		"title":".game-info-container&&Text;.customer-navbar-nav li&&Text",
+        //"title":".game-info-container&&Text;.customer-navbar-nav li&&Text"
+		"title":".game-info-container&&Text",
 		//"img":"img&&src",
 		//"desc":";;;text-right&&Text;text-left&&Text",
 	    //"content":"list-group li&&Text",
