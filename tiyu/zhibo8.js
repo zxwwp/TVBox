e6ad51d0f41b11673c0d7d499a1a95993d39e8f7
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-14 06:44:57 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -1,10 +1,7 @@
 var rule = {
-    title:'JRS直播',
+    title:'zhibo8',
     host:'https://www.zhibo8.net',
     url:'/fyclass',
-    searchUrl:'',
-    searchable:0,
-    quickSearch:0,
     class_name:'全部',
     class_url:'/',
     headers:{
@@ -16,7 +13,14 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.main-left .match-list li;div:gt(1):lt(3)&&Text;img&&src;.col&&Text;a&&href',
-    二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:'',tab_text:'',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.match-item');tabs.forEach(function(it){var pz=pdfh(it,'.name:eq(1)&&Text');var ps=pdfh(it,'.name:eq(0)&&Text');var pk=pdfh(it,'.name:eq(2)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.lab_time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    
+    二级:{
+		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",//类型 时间
+		"desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",  //演员;导演
+		"content":".sub_list ul&&Text",  // 主要信息
+		"tabs":"js:TABS=['直播信号源']",
+		"lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
+	},
     搜索:'',
 }
