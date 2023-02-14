87d1069f449f1319ebbdf75b6e36ded5258cde78
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-14 00:47:51 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -15,9 +15,9 @@ var rule = {
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.group-game-item');tabs.forEach(function(it){var pz=pdfh(it,'.text-right&&Text');var ps=pdfh(it,'.game-type&&Text');var pk=pdfh(it,'.text-left&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.category-game-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
 		"title":".game-info-container&&Text;.customer-navbar-nav li&&Text",
-		"img":"img&&src",
-		"desc":";;;text-right&&Text;text-left&&Text",
-	    "content":"list-group li&&Text",
+		//"img":"img&&src",
+		//"desc":";;;text-right&&Text;text-left&&Text",
+	    //"content":"list-group li&&Text",
         "tabs":"js:TABS=['直播信号源']",
 	    "lists":"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)});"
 
