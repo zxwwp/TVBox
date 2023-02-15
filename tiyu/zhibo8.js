58e238189d96e45fc2b5342715beb26754559e3a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-15 07:13:54 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -14,13 +14,6 @@ var rule = {
     double:false,
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.listBox');tabs.forEach(function(it){var pz=pdfh(it,'.myList div:gt(1)&&Text');var ps=pdfh(it,'.matchType&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.timer&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
-    
-    二级:{
-		"title":".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",//类型 时间
-		"desc":";;;.lab_team_home&&Text;.lab_team_away&&Text",  //演员;导演
-		"content":".sub_list ul&&Text",  // 主要信息
-		"tabs":"js:TABS=['直播信号源']",
-		"lists":"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});",
-	},
+    二级:{title:'.msg ul&&Text;.msg ul&&Text',img:'img:eq(2)&&src',desc:';;;.nameOne&&Text;.nameTwo&&Text',content:'._t&&Text',tabs:"js:TABS=['直播信号源']",lists:'.checkChannel li:gt(0)',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
