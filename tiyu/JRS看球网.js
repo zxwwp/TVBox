51ce23e880b5b882584c6e7481cdadcda754f724
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-15 01:57:42 +0000

    update tiyu/JRS看球网.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRS看球网.js
+++ tiyu/JRS看球网.js
@@ -16,7 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:'.todayMatch .contenTab;.myList div:gt(1)&&Text;img&&src;.status&&Text;a&&href',
+    一级:'.todayMatch .contenTab;.myList div:gt(0)&&Text;img&&src;.status&&Text;a&&href',
     //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.contenTab');tabs.forEach(function(it){var pz=pdfh(it,'.myList div:gt(1)&&Text');var ps=pdfh(it,'.matchType&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.timer&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     二级:{title:'.msg ul&&Text;.msg ul&&Text',img:'img:eq(2)&&src',desc:';;;.nameOne&&Text;.nameTwo&&Text',content:'._t&&Text',tabs:"js:TABS=['直播信号源']",lists:'.checkChannel li:gt(0)',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
