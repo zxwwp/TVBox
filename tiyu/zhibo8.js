05c7227d5fd636741ade449e9b07989afc24f44d
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-15 07:56:38 +0000

    update tiyu/zhibo8.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/zhibo8.js
+++ tiyu/zhibo8.js
@@ -1,6 +1,6 @@
 var rule = {
     title:'zhibo8',
-    host:'https://sports.qq.com/kbsweb/',
+    host:'https://www.zhibo8.net/live_1.html?project=1&game=0',
     url:'/fyclass',
     class_name:'全部',
     class_url:'/',
@@ -13,7 +13,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.title');tabs.forEach(function(it){var pz=pdfh(it,'.li div:gt(2)&&span');var ps=pdfh(it,'.col-name&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.col-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.   basketball');tabs.forEach(function(it){var pz=pdfh(it,'.li div:gt(2)&&span');var ps=pdfh(it,'.col-name&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.col-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     二级:{title:'.msg ul&&Text;.msg ul&&Text',img:'img:eq(2)&&src',desc:';;;.nameOne&&Text;.nameTwo&&Text',content:'._t&&Text',tabs:"js:TABS=['直播信号源']",lists:'.checkChannel li:gt(0)',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
