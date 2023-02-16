90b0b1f1cd2813fe119001fd3a058408215ee22b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-16 08:03:30 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -2,11 +2,12 @@ var rule = {
     title:'926直播',
     host:'http://www.926.tv',
     //备用网址：http://www.928.tv，http://www.310.tv
-    url:'/?s=0&t=1&a=fyclass&g=fypage',
-    class_name:'篮球&足球&体育',
-    class_url:'2&1&3',         
-    //class_name:'全部',
-    //class_url:'/',
+    //url:'/?s=0&t=1&a=fyclass&g=fypage',
+    //class_name:'篮球&足球&体育',
+    //class_url:'2&1&3', 
+    url:'/fypage',        
+    class_name:'全部',
+    class_url:'/',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -17,7 +18,10 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-   一级:'.list_content a;.jiabifeng&&p:lt(2)&&Text;img&&op-zfr-a-g;a&&t-nzf-o;a&&href',
-   二级:'*', 
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.match-link');tabs.forEach(function(it){var pz=pdfh(it,'.left-team&&Text');var ps=pdfh(it,'.mobile-type&&Text');var pk=pdfh(it,'.right-team&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.match-start-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:"js:TABS=['直播信号:']",lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
+    
+  // 一级:'.list_content a;.jiabifeng&&p:lt(2)&&Text;img&&op-zfr-a-g;a&&t-nzf-o;a&&href',
+   //二级:'*', 
     搜索:'',
 }
