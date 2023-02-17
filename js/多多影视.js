df14a685f2dd3813239eb574069d1402e4c15027
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-17 01:39:08 +0000

    add js/多多影视.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ js/多多影视.js
@@ -0,0 +1,9 @@
+muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
+var rule = Object.assign(muban.首图2,{
+    title:'多多影视',
+    ali_token:'{{ali_token}}',
+    bili_cookie:'{{bili_cookie}}',
+    host:'http://www.doudd.net',
+    url:'/vodtype/fyclass-fypage.html',
+    searchUrl:'/search/**----------fypage---.html',
+});
