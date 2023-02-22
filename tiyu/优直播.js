393f59a8f510bfb2c151d1aa7a598ddd5b3ac0f1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 02:29:16 +0000

    update tiyu/优直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -19,7 +19,7 @@ var rule = {
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.item');tabs.forEach(function(it){var  pz=pdfh(it,'.name:eq(0)&&Text');var ps=pdfh(it,'.league&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
      二级:{
-          title:'a&&title',  //片名;类型 时间
+          title:'.title&&Text',  //片名;类型 时间
           desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',  //// 演员;导演
           content:'a&&title',  ////主要信息
           tabs:"js:TABS=['【直播源】']",
