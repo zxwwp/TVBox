0293d543a84d6e7cdb9e52a37ae84c8a6f770042
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 03:13:19 +0000

    update tiyu/优直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -28,5 +28,5 @@ var rule = {
           //lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'
          },
 
-     搜索:'json:cObj.cList;*;*;*;_id',  
+     搜索:'js:cObj.cList;*;*;*;_id',  
     }
