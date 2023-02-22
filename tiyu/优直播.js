f140e4adee6b9d48aec2c2f734e9e6a827d1b698
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 02:27:33 +0000

    update tiyu/优直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -21,7 +21,7 @@ var rule = {
      二级:{
           title:'.title&&Text',  //片名;类型 时间
           desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',  //// 演员;导演
-          content:'.title&&Text',  ////主要信息
+          content:'a&&title',  ////主要信息
           tabs:"js:TABS=['【直播源】']",
           lists:'div.sig-list a ncp',
           list_text:'a&&nofollow',
