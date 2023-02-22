dba75e7fac2db4238d2643d398ecd88c1298b1ea
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 08:27:36 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -5,7 +5,7 @@ var rule = {
     url:'/?s=0&t=1&a=fyclass&g=fypage',
     class_name:'热门&篮球&足球&体育',
     class_url:'/&2&1&3', 
-    //homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
+    homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -18,7 +18,7 @@ var rule = {
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.clearfix.jiabifeng');tabs.forEach(function(it){var pz=pdfh(it,'.zhudui_wuyd&&Text');var ps=pdfh(it,'.eventtime_wuy.eventtime&&Text');var pk=pdfh(it,'.team.kedui&&Text');var img=pd(it,'img&&op-zfr-a-g');var timer=pdfh(it,'.t-nzf-o&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
    二级:{
-        title:'.live-title&&Text;.live-title&&Text',
+        title:'.date-p&&Text;.eventtime_wuy.eventtime&&Text',
         desc:';;;.zhudui_wuyd&&Text;.team.kedui&&Text',
         tabs:"js:TABS=['【直播源】']",
         lists:'#singldl dd',
