fb8428ced21cb9d0dead843f995d0f694f6e655d
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 02:04:49 +0000

    update tiyu/抓饭体育.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/抓饭体育.js
+++ tiyu/抓饭体育.js
@@ -7,8 +7,8 @@ var rule = {
     title:'抓饭体育',
     host:'https://www.zhuafan.tech',
     url:'/sports-home/category/fyclass',
-    class_name:'全部&足球&篮球&羽乒&台球&棒球&户外&搏击&综合&棋盘&电竞&网球&排球&聊天&原声',
-    class_url:'all&Football&Basketball&Badminton&Billiards&Baseball&Outdoors&Wrestling&Others&Boardgame&Popular&Tennis&Volleyball&Chat&Acoustic',
+    class_name:'全部&足球&篮球&羽乒&排球&台球&网球&棋盘&棒球&搏击&综合&电竞',
+    class_url:'all&Football&Basketball&Badminton&Volleyball&Billiards&Tennis&Boardgame&Baseball&Wrestling&Others&Popular',
 	homeUrl:'/sports-home/category/all',//网站的首页链接,用于分类获取和推荐获取
     detailUrl:'https://m.zhuafan.tech/fyid',//二级详情拼接链接(json格式用)
     searchUrl:'/live-search/search/query/data?keyword=**&page=fypage&num=&searchType=all&uid=null&from=pc',
