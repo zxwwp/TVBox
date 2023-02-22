c30566d98856f8884f11ccc33c824ad00b235905
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-22 09:02:43 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -2,8 +2,7 @@ var rule = {
     title:'926直播',
     host:'http://www.926.tv',
     //备用网址：http://www.928.tv，http://www.310.tv
-    //url:'/?s=0&t=1&a=fyclass&g=fypage',
-     url:'/fyclass',
+    url:'/fyclass',
     class_name:'热门&篮球&足球&体育',
     class_url:'/&2&1&3', 
     homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
