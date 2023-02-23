f265a4ae7e272236bcb52c8f5778f72fa0a5c89c
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-23 01:14:55 +0000

    update tiyu/优直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -19,8 +19,9 @@ var rule = {
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.item.cd ');tabs.forEach(function(it){var  pz=pdfh(it,'.name:eq(0)&&Text');var ps=pdfh(it,'.league&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
      二级:{
-          title:'.arrow:it(2)&&Text;.league&&Text',  //片名;类型 时间
+          title:'.matches li:it(2)&&Text;.league&&Text',  //片名;类型 时间
           desc:';;;.name:eq(0)&&Text;.name:eq(1)&&Text',  //// 演员;导演
+          content:".sub_list ul&&Text", //主要信息
           tabs:"js:TABS=['【直播源】']",
           lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sig-list.a.ncp&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'.nofollow&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
           //lists:'.sig-list.a.ncp a',
