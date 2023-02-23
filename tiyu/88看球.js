5292b3d9f251d02c2d1e7f96f3149c1eba0235cd
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-23 07:46:53 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -1,11 +1,10 @@
 var rule = {
     title:'88看球',
     host:'http://www.88kanqiu.live',
-    url:'/match/fyclass/live',
-    //url:'/fyclass',
+    url:'/fyclass',
     class_name:'全部',
     class_url:'/', 
-    class_parse:'.nav-pills li;a&&Text;a&&href;/match/(\\d+)/live',//动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
+    //class_parse:'.nav-pills li;a&&Text;a&&href;/match/(\\d+)/live',//动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
     headers:{
         'User-Agent':'PC_UA'
     },
