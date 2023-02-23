56619670722e13fb107d5730b4e433484a3b36a1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-23 01:07:06 +0000

    update tiyu/优直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/优直播.js
+++ tiyu/优直播.js
@@ -22,7 +22,7 @@ var rule = {
           title:'.arrow:it(2)&&Text;.league&&Text',  //片名;类型 时间
           desc:';;;.name:eq(0)&&Text;.name:eq(1)&&Text',  //// 演员;导演
           tabs:"js:TABS=['【直播源】']",
-          lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});"
+          lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sig-list.a.ncp&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'.nofollow&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
           //lists:'.sig-list.a.ncp a',
           //list_text:'a&&Text',
           //list_url:'a&&href'
