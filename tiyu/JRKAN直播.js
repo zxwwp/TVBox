c9f78e4071e384a10601958ca24dc4f4ba61c1bc
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-23 06:38:50 +0000

    update tiyu/JRKAN直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRKAN直播.js
+++ tiyu/JRKAN直播.js
@@ -17,8 +17,9 @@ var rule = {
     url:'/fyclass',
     class_name:'全部&足球&篮球&其他',
     class_url:'all&football&basketball&other', 
+    homeUrl:'/', 
     //class_name:'全部',
-    class_url:'/',
+    //class_url:'/',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
