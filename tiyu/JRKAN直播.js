93c4a109a49e2100e8b97c84f7b4045a3da0463f
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-23 04:08:32 +0000

    update tiyu/JRKAN直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRKAN直播.js
+++ tiyu/JRKAN直播.js
@@ -15,7 +15,9 @@ var rule = {
 	// JRKAN备用域名:www.jrkankan.com / www.jrkan365.com / jrsyyds.com / www.jryyds.com / jrskan.com / jrsbxj.com
 	// JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
-    class_name:'全部',
+    //class_name:'全部&篮球&足球',
+    //class_url:'0&18&17', 
+    //class_name:'全部',
     class_url:'/',
     headers:{
         'User-Agent':'MOBILE_UA'
