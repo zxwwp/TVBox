d64fb468bc7c6c940b2647a965dfcceb236adb76
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-26 02:30:22 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -17,7 +17,7 @@ var rule = {
     
     二级:{
 		title:".sbtsw spna&&Text",//类型 时间
-		desc:";;;.sbtsw022&&Text;.sbtsw023&&Text",  //演员;导演
+		desc:";;;.teamName.nameOne&&Text;.teamName.nameTwo&&Text",  //演员;导演
 		content:".timer&&Text",  // 主要信息
 		tabs:"js:TABS=['【直播源】']",
 		lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.down-site');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'nofollow&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
