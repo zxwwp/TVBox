fbd1208732f73c9495d5733baa32003dd9368098
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-27 07:36:21 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -16,7 +16,7 @@ var rule = {
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.tit ');tabs.forEach(function(it){var pz=pdfh(it,'.sbtsw022&&Text');var ps=pdfh(it,'.sbtsw spna&&Text');var pk=pdfh(it,'.sbtsw023&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.sbtsw&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
    二级:{title:"#list_1 li div:gt(2):lt(4)&&Text",
    desc:";;;.sbtsw022&&Text;.sbtsw023&&Text",
-   content:"div.downinfo-detail:ep(2)&&Text",
+   content:"#list_1 li div:gt(2):lt(4)&&Text",
    tabs:"js:TABS=['【直播源】']",
    lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'#down-site:lt(1) li');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'.list&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"},
    搜索:'',
