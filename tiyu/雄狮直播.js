9f1cc29482ea51dcbb1ab391c9ca7f84727d9954
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-27 02:40:17 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -15,7 +15,7 @@ var rule = {
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.tit ');tabs.forEach(function(it){var pz=pdfh(it,'.sbtsw022&&Text');var ps=pdfh(it,'.sbtsw spna&&Text');var pk=pdfh(it,'.sbtsw023&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.sbtsw&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
-        title:"p.zb_zzt&&Text",
+        title:"li.timer&&Text",
         desc:";;;.nameOne&&Text;.nameTwo&&Text",
         content:"div.msg&&Text",
         tabs:"js:TABS=['【直播源】']",
