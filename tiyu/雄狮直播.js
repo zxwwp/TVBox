6eeba20dc89da7df060d074b7b3228b43397e8bf
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-27 01:52:25 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -19,7 +19,7 @@ var rule = {
         desc:";;;.nameOne&&Text;.nameTwo&&Text",
         content:".msg ul&&Text",
         tabs:"js:TABS=['【直播源】']",
-        lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'#downinfo-detail&&p');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'rel&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
+        lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'#downinfo-detail&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'rel&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
         },
    搜索:'',
 }
