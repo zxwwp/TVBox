cfe24ad9ed45df924ef2a01c639411a158cf7b7b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-02-27 08:12:39 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -6,7 +6,6 @@ var rule = {
     class_url:'/', 
     //class_parse:'.nav-pills li;a&&Text;a&&href;/match/(\\d+)/live',//动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
     headers:{
-        //'User-Agent':'PC_UA'
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
