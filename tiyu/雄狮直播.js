161a03646827c41922a0e590ef9c08eb405f7820
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-02 01:13:40 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -7,23 +7,21 @@ var rule = {
     searchable:2,//是否启用全局搜索,
     quickSearch:0,//是否启用快速搜索,
     headers:{
-        //'User-Agent':'MOBILE_UA'
-         'User-Agent':'PC_UA',
+        'User-Agent':'MOBILE_UA'
     },
     //timeout:5000,
     play_parse:true,
     lazy:"",
     limit:6,
     推荐:'*',
-    一级:'.list_0 .tit;.sbtsw&&Text;.lazy&&src;.sbtsw022&&Text;a&&href',
-    //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.tit ');tabs.forEach(function(it){var pz=pdfh(it,'.sbtsw022&&Text');var ps=pdfh(it,'.sbtsw spna&&Text');var pk=pdfh(it,'.sbtsw023&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.sbtsw&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.tit ');tabs.forEach(function(it){var pz=pdfh(it,'.sbtsw022&&Text');var ps=pdfh(it,'.sbtsw spna&&Text');var pk=pdfh(it,'.sbtsw023&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.sbtsw&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
    二级:{
        title:'.live-title&&Text;.live-title&&Text',
        img:'img:eq(1)&&src',
        desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',
        content:'.live-info&&Text',
        tabs:"js:TABS=['【直播源】']",
-       lists:'#downinfo-detail:eq(#down-site) a',
+       lists:'.downinfo-detail .down-site a',
        list_text:'a&&Text',
        list_url:'a&&data-url'
     },
