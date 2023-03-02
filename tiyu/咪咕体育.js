bba1109db1269a358c8f04f5bd844bb262d79ae4
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-02 07:37:00 +0000

    update tiyu/咪咕体育.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/咪咕体育.js
+++ tiyu/咪咕体育.js
@@ -13,7 +13,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.date-list-item');tabs.forEach(function(it){var pz=pdfh(it,'.team-name&&Text');var ps=pdfh(it,'.game-type&&Text');var pk=pdfh(it,'.team-name boss&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.date&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.date-list-item-box');tabs.forEach(function(it){var pz=pdfh(it,'.team-name&&Text');var ps=pdfh(it,'.game-type&&Text');var pk=pdfh(it,'.team-name boss&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.date&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     
     二级:{
 		title:".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",
