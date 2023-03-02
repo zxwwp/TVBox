b06e9cda76d7004944f5beff42554878e0290b5a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-02 01:16:55 +0000

    update tiyu/雄狮直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/雄狮直播.js
+++ tiyu/雄狮直播.js
@@ -17,11 +17,10 @@ var rule = {
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.tit ');tabs.forEach(function(it){var pz=pdfh(it,'.sbtsw022&&Text');var ps=pdfh(it,'.sbtsw spna&&Text');var pk=pdfh(it,'.sbtsw023&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.sbtsw&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
    二级:{
        title:'.live-title&&Text;.live-title&&Text',
-       img:'img:eq(1)&&src',
        desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',
        content:'.live-info&&Text',
        tabs:"js:TABS=['【直播源】']",
-       lists:'.downinfo-detail .down-site a',
+       lists:'div.downinfo-detail p.down-site a',
        list_text:'a&&Text',
        list_url:'a&&data-url'
     },
