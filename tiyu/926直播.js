eedac830ad9aef457654ac33cc95e06b622d0c77
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-03 03:15:21 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -15,7 +15,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-     一级:'.list_content a;.jiabifeng:lt(5)&&Text;img&&op-zfr-a-g;p.eventtime_wuy.eventtime&&Text;a&&href', 
+     一级:'.list_content a;.jiabifeng:lt(2)&&Text;img&&op-zfr-a-g;p.eventtime_wuy.eventtime&&Text;a&&href', 
     //一级:'.list_content a;.jiabifeng&&p:lt(2)&&Text;img&&op-zfr-a-g;p.eventtime_wuy.eventtime&&Text;a&&href', 
     二级:'*', 
      //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.clearfix.jiabifeng');tabs.forEach(function(it){var pz=pdfh(it,'.zhudui_wuyd&&Text');var ps=pdfh(it,'p.eventtime&&Text');var pk=pdfh(it,'.team.kedui&&Text');var img=pd(it,'img&&op-zfr-a-g');var timer=pdfh(it,'p.eventtime&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
