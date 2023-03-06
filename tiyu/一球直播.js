6a613e7193f5f2b69a524bba0aa9be943747a757
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-06 08:21:19 +0000

    update tiyu/一球直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/一球直播.js
+++ tiyu/一球直播.js
@@ -1,12 +1,12 @@
 var rule = {
     title:'一球直播',
-    host:'https://yiqiulive.com/live/list/',
+    host:'https://yiqiulive.com',
     url:'/fyclass',     //网站的分类页面链接
     //class_name:'赛事直播✨注意时间',       //静态分类名称拼接
     //class_url:'/',    //静态分类标识拼接
     //homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
     class_name:'足球&篮球',       //静态分类名称拼接
-    class_url:'1&2',    //静态分类标识拼接
+    class_url:'/live/list/1&/live/list/2',    //静态分类标识拼接
     homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
     headers:{
         'User-Agent':'MOBILE_UA',
