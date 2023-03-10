d5dc56bed38178a0dab6b267fc744f85afea166d
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-10 01:10:54 +0000

    update tiyu/一球直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/一球直播.js
+++ tiyu/一球直播.js
@@ -18,17 +18,11 @@ var rule = {
     limit:6,    // 首页推荐显示数量
     double:false,    //是否双层列表定位,默认false
     推荐:'*',
+     一级:'._jN9fi2tGjt div;._jN9fi2tGjt&&Text;img&&src;_bUuy3C5Buw&&Text;a&&href', 
+    二级:'*', 
       // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&._bUuy3C5Buw');tabs.forEach(function(it){var  pz=pdfh(it,'._Iv0sSLsVT7&&Text');var ps=pdfh(it,'._tZhrfoDUd1&&Text');;var img=pd(it,'img&&src');var timer=pdfh(it,'._HtlGr9Vxvg&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&._bUuy3C5Buw');tabs.forEach(function(it){var  pz=pdfh(it,'._Iv0sSLsVT7&&Text');var ps=pdfh(it,'._tZhrfoDUd1&&Text');;var img=pd(it,'img&&src');var timer=pdfh(it,'._HtlGr9Vxvg&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
-     二级:{
-          title:'.item.cd li:lt(5)&&Text;.league&&Text',  //片名;类型 时间
-          desc:';;;.name:eq(0)&&Text;.name:eq(1)&&Text',  //// 演员;导演
-          content:".play&&a", //主要信息
-          tabs:"js:TABS=['【直播源】']",
-          lists:'.sig-list.a.ncp a',
-          list_text:'a&&Text',
-          list_url:'a&&href'
-         },
+     //二级:{title:'.item.cd li:lt(5)&&Text;.league&&Text',desc:';;;.name:eq(0)&&Text;.name:eq(1)&&Text',content:".play&&a",tabs:"js:TABS=['【直播源】']",lists:'.sig-list.a.ncp a',list_text:'a&&Text',list_url:'a&&href'},
      搜索:'',  
     }
