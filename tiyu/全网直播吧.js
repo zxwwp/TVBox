b1479ca04d3a7aa4234b52def3366a2437e1326a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-17 07:06:56 +0000

    update tiyu/全网直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/全网直播吧.js
+++ tiyu/全网直播吧.js
@@ -18,7 +18,7 @@ var rule = {
     // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
     推荐:'*',
-    一级:'.main-left ul;li div:ge(1):lt(3)&&Text;img&&src;div&&col-time;a&&href',
+    一级:'.main-left li;li div:ge(1):lt(3)&&Text;img&&src;div&&col-time;a&&href',
     二级:'*', 
 // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&ul.match-list li');tabs.forEach(function(it){var  pz=pdfh(it,'.col-home.flex&&Text');var ps=pdfh(it,'.col-name&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.col-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
