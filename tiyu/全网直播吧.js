c8acc606156f12f6f0015570d847136caf5524fc
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-17 06:42:12 +0000

    update tiyu/全网直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/全网直播吧.js
+++ tiyu/全网直播吧.js
@@ -5,7 +5,8 @@ var rule = {
     class_parse:'.nav-item li;a&&Text;a&&href',//动态分类获取 列表;标题;链接;正则提取 不需要正则的时候后面别加分号
         
     headers:{
-         'User-Agent':'MOBILE_UA','User-Agent':'PC_UA'
+         'User-Agent':'MOBILE_UA',
+        "Cookie": "searchneed=ok"
     },//网站的请求头,完整支持所有的,常带ua和cookies
     timeout:5000,//网站的全局请求超时,默认是3000毫秒
     play_parse:true,  // 服务器解析播放
@@ -15,8 +16,9 @@ var rule = {
     // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     // 如果是双层定位的话,推荐的第2段分号代码也是第2层定位列表代码
     推荐:'*',
-    一级:'.list-unstyled    basketball;li div:ge(1):lt(3)&&Text;img&&src;div&&col-time;a&&href',
+    //一级:'.list-unstyled    basketball;li div:ge(1):lt(3)&&Text;img&&src;div&&col-time;a&&href',
 // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.match-item');tabs.forEach(function(it){var  pz=pdfh(it,'.col-home.flex&&Text');var ps=pdfh(it,'.col-name&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.col-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
  // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
     // 二级 title: 片名;类型
     // 二级 desc: 主要信息;年代;地区;演员;导演
