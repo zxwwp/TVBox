9b5c1ff0a5b3340bc2c1a556716d0cff9504e4fe
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-22 08:08:39 +0000

    update tiyu/玩球直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/玩球直播.js
+++ tiyu/玩球直播.js
@@ -13,16 +13,14 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-         一级:'.live-list .zuqiu;.title&&Text;img&&src;.category&&Text;a&&href', 
-    二级:'*', 
-    //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.live-list li');tabs.forEach(function(it){var pz=pdfh(it,'.title&&Text');var ps=pdfh(it,'.category&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
-    //二级:{
-       // title:".livetime-title&&Text;.category&&Text",//类型 时间
-		//desc:";;;.title&&Text",  //演员;导演
-		//content:".live-description&&Text",  // 主要信息
-		//tabs:"js:TABS=['【直播源】']",
-		//lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.channel-content:eq(2)&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'a&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.live-list li');tabs.forEach(function(it){var pz=pdfh(it,'.title&&Text');var ps=pdfh(it,'.category&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    二级:{
+        title:".livetime-title&&Text;.category&&Text",//类型 时间
+		desc:";;;.title&&Text",  //演员;导演
+		content:".live-description&&Text",  // 主要信息
+		tabs:"js:TABS=['【直播源】']",
+		lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.channel-content .xh&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'a&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
          // lists:"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)})"
-    //},
+        },
     搜索:'',
 }
