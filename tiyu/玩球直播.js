7b4f8e2da55fbe65c0c4477156fca1ae0b2d5d4e
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-22 08:19:04 +0000

    update tiyu/玩球直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/玩球直播.js
+++ tiyu/玩球直播.js
@@ -17,7 +17,7 @@ var rule = {
     二级:{
         title:".livetime-title&&Text;.category&&Text",//类型 时间
 		desc:";;;.title&&Text",  //演员;导演
-		content:".live-description&&Text",  // 主要信息
+		//content:".live-description&&Text",  // 主要信息
 		tabs:"js:TABS=['【直播源】']",
 		lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'div.xh&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'.xh&&Text');let url=pd(it,'a&&href');return name+'$'+url});LISTS.push(d)});"
          // lists:"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)})"
