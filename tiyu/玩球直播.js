9c69d970e40595b328f862d85728d331e13cb3b2
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-22 06:57:43 +0000

    update tiyu/玩球直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/玩球直播.js
+++ tiyu/玩球直播.js
@@ -13,7 +13,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.live-list li');tabs.forEach(function(it){var pz=pdfh(it,'.title&&Text');var ps=pdfh(it,'.category&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.status&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.live-list li');tabs.forEach(function(it){var pz=pdfh(it,'.title&&Text');var ps=pdfh(it,'.category&&Text');var img=pd(it,'img&&https://gitee.com/xl04wy22/tvbox/raw/master/jpg/pngpai.com_football-player-631531677974313.png');var timer=pdfh(it,'.status&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     二级:{
         title:".live-list li:lt(2)&&Text;.live-list li:eq(0)&&Text",//类型 时间
 		desc:";;;.lab_team_home&&Text;.lab_team_away&&Text",  //演员;导演
