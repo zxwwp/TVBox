3b6c02b26aa76e929ede593bbc8dcbbd8fab28e5
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-24 08:05:07 +0000

    update tiyu/快体育.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/快体育.js
+++ tiyu/快体育.js
@@ -18,7 +18,7 @@ var rule = {
     推荐:'*',
    //一级:'.van-list div;.jiabifeng&&p:lt(2)&&Text;img&&op-zfr-a-g;p.eventtime_wuy.eventtime&&Text;a&&href', 
     //二级:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.pcDom div');tabs.forEach(function(it){var pz=pdfh(it,'.pcDom_left_right_row-1&&Text');var ps=pdfh(it,'.divisionTime&&Text');var pk=pdfh(it,'.row-1&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.match-start-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.new-infobox-content div');tabs.forEach(function(it){var pz=pdfh(it,'.pcDom_left_right_row-1&&Text');var ps=pdfh(it,'.divisionTime&&Text');var pk=pdfh(it,'.row-1&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.match-start-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
         title:'.live-title&&Text;.live-title&&Text',
         desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',
