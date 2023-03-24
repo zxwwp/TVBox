78ea4b64ed5e02e15f62ad7ebf2e9645af72f7e3
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-24 01:21:05 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -24,9 +24,11 @@ var rule = {
         //content:'.weikan&&Text',
         content:'.biaoti&&Text',
         tabs:"js:TABS=['【直播源】']",
-        lists:'.singldl dd',
-        list_text:'.diss&&Text',
-        list_url:'a&&data-url'
+        lists:"js:LISTS=[];input=input+'-url';let html=request(input);let data=JSON.parse(html);TABS.forEach(function(tab){let m3u=data;let d=m3u.map(function(it){return it.name+'$'+it.url});LISTS.push(d)})"
+    
+        //lists:'.singldl dd',
+        //list_text:'.diss&&Text',
+        //list_url:'a&&data-url'
         },
     搜索:'',
 }
