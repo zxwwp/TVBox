158f13e880cbfab40e184b7cdb0dab1d8e2ea12a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-26 00:03:39 +0000

    update tiyu/926直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/926直播.js
+++ tiyu/926直播.js
@@ -2,9 +2,10 @@ var rule = {
     title:'926直播',
     host:'http://www.926.tv',
     //备用网址：http://www.928.tv，http://www.310.tv
-    url:'/fyclass',
+    //url:'/fyclass',
     //class_name:'热门&篮球&足球&体育',     //静态分类名称拼接
     //class_url:'0&1&2&3',    //静态分类标识拼接
+    url:'/?s=0&t=1&a=fyclass&g=fypage',
     class_name:'赛事直播✨注意时间',
     class_url:'/', 
     headers:{
@@ -16,6 +17,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
+    //一级:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
     //一级:'.list_content a;.jiabifeng&&p:lt(2)&&Text;img&&op-zfr-a-g;p.eventtime_wuy.eventtime&&Text;a&&href', 
     //二级:'*', 
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.list_content a');tabs.forEach(function(it){var pz=pdfh(it,'.zhudui_wuyd&&Text');var ps=pdfh(it,'.diss&&Text');var pk=pdfh(it,'.team.kedui&&Text');var img=pd(it,'img&&op-zfr-a-g');var timer=pdfh(it,'p.eventtime&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
