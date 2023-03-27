243a528e882885c50853fa6affa20c67587117d2
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-27 07:19:46 +0000

    update tiyu/好球直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/好球直播吧.js
+++ tiyu/好球直播吧.js
@@ -2,7 +2,7 @@ var rule = {
     title:'好球直播吧',
     host:'https://www.haoqiuzhibo.com/',
     url:'/fyclass',     //网站的分类页面链接
-    class_name:'全部直播&足球直播&篮球直播&综合直播',       //静态分类名称拼接
+    class_name:'全部&足球&篮球&综合',       //静态分类名称拼接
     class_url:'/&live/zuqiu/&live/lanqiu/&live/tiyu/',    //静态分类标识拼接
     homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
     headers:{
