ed5ea3304435b42fd8bea6e8ace7c309295ea7b0
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-27 07:18:51 +0000

    update tiyu/好球直播吧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/好球直播吧.js
+++ tiyu/好球直播吧.js
@@ -16,7 +16,7 @@ var rule = {
     double:false,    //是否双层列表定位,默认false
     推荐:'*',
       // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&li.bg_color');tabs.forEach(function(it){var  pz=pdfh(it,'.zb_neirong a:gt(2)&&Text');var ps=pdfh(it,'.zb_neirong a&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'span.date&&Text');var url=pd(it,'.zb_pd a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&li.bg_color');tabs.forEach(function(it){var  pz=pdfh(it,'.zb_neirong a:gt(2)&&Text');var ps=pdfh(it,'.zb_neirong a&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'span.date&&Text');var url=pd(it,'.zb_pd a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
      二级:{
           title:'p.timeP&&Text;.score_name a&&Text',  //片名;类型 时间
