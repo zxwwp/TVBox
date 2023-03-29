8bb0e643ae1e5fd300f321d84885d8ad677d3e7a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-29 01:35:20 +0000

    update tiyu/高清直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/高清直播.js
+++ tiyu/高清直播.js
@@ -15,7 +15,7 @@ var rule = {
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&tr.against');tabs.forEach(function(it){var pz=pdfh(it,'tr.against td:gt(3)&&Text');var ps=pdfh(it,'a&&Text');var pk=pdfh(it,'.category&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'td.matcha td&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
-        //title:".title-content hl&&Text",//类型 时间
+        title:".title-content hl&&Text;a&&Text",//类型 时间
 		//desc:";;;.title&&Text",  //演员;导演
 		//content:".title-content hlg&&Text",  // 主要信息
 		tabs:"js:TABS=['【直播源】']",
