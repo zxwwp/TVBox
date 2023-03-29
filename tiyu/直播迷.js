5719292d7077bcb016b27f62d1a2637a333d0480
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-03-29 03:17:50 +0000

    update tiyu/直播迷.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/直播迷.js
+++ tiyu/直播迷.js
@@ -19,7 +19,7 @@ var rule = {
 		//desc:";;;.title&&Text",  //演员;导演
 		//content:".title-content hlg&&Text",  // 主要信息
 		tabs:"js:TABS=['【直播源】']",
-		lists:'.panel.panel-default a',
+		lists:'div.panel-default a',
         list_text:'a&&Text',
         list_url:'a&&href'
         },
