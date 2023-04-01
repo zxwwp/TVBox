58f88a7ec62120e79ab5b11111797a5b2348630f
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-01 01:16:08 +0000

    add js/速搜影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ js/速搜影院.js
@@ -0,0 +1,31 @@
+var rule = {
+    title:'速搜影院',
+    host:'https://www.sstv.pw/',
+    url:'/fyclass',     //网站的分类页面链接
+    class_name:'电影&剧集&综艺&动漫&纪录',       //静态分类名称拼接
+    class_url:'vodshow-1&vodshow-2&vodshow-3&vodshow-4&vodshow-21',    //静态分类标识拼接
+    homeUrl:'/',       //网站的首页链接,用于分类获取和推荐获取
+    headers:{
+        'User-Agent':'MOBILE_UA',
+        "Cookie": "searchneed=ok"
+    },     //网站的请求头,完整支持所有的,常带ua和cookies
+    timeout:5000,     //网站的全局请求超时,默认是3000毫秒
+    play_parse:true,    // 服务器解析播放
+    lazy:'',    // 自定义免嗅
+    limit:6,    // 首页推荐显示数量
+    double:false,    //是否双层列表定位,默认false
+    推荐:'*',
+      // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&ul.item.cd');tabs.forEach(function(it){var  pz=pdfh(it,'.name:eq(0)&&Text');var ps=pdfh(it,'.league&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.time&&Text');var url=pd(it,'li.play a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
+     二级:{
+          title:'.sig-vs-txt span&&Text;.sig-vs-txt b&&Text',  //片名;类型 时间
+          desc:';;;.ht a&&Text;.gt a&&Text',  //// 演员;导演
+          content:".team&&Text", //主要信息
+          tabs:"js:TABS=['【直播源】']",
+          lists:'.sig-list.a.ncp a:gt(0):lt(17)',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
+          list_text:'a&&Text',
+          list_url:'a&&href'
+         },
+     搜索:'',  
+    }
