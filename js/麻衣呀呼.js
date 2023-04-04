84cc310cdc705aad1e38460673aafbefe9574c3b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-04 07:09:54 +0000

    update js/麻衣呀呼.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/麻衣呀呼.js
+++ js/麻衣呀呼.js
@@ -9,11 +9,12 @@ var rule = {
         'User-Agent':'MOBILE_UA',
         "Cookie": "searchneed=ok"
     },     //网站的请求头,完整支持所有的,常带ua和cookies
-    timeout:5000,     //网站的全局请求超时,默认是3000毫秒
+   searchable:2,//是否启用全局搜索,
+	quickSearch:0,//是否启用快速搜索,
+	filterable:0,//是否启用分类筛选,
     play_parse:true,    // 服务器解析播放
     lazy:'',    // 自定义免嗅
     limit:6,    // 首页推荐显示数量
-    double:false,    //是否双层列表定位,默认false
     推荐:'.modulemod;.cf.listwap&&.gcol.l;a&&title;div&&data-original;a&&href',
       // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.gcol.l');tabs.forEach(function(it){var  pz=pdfh(it,'a.plusclk&&Text');var ps=pdfh(it,'span.packscore&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'.inlist.ec-jabj.eclazy&&div&&data-original');var timer=pdfh(it,'.&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
