24ca731bd60f21f98b4c53b725d5c1d268adb540
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-07 07:34:21 +0000

    update js/看看剧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/看看剧.js
+++ js/看看剧.js
@@ -13,10 +13,10 @@ var rule={
     double:true, // 推荐内容是否双层定位
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.stui-vodlist__box');tabs.forEach(function(it){var  pz=pdfh(it,'.stui-vodlist__detail h4&&Text');var ps=pdfh(it,'.pic-text b&&Text');var img=pd(it,'a&&data-original');var timer=pdfh(it,'.&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
     二级:{
-        "title":".stui-content__detail h1&&Text;.stui-content__detail p:gt(0)&&Text",
+        "title":".stui-content__detail h1&&Text;.stui-content__detail p:gt(-1)&&Text",
         "img":".lazyload&&data-original",
-        "desc":";;;.module-info-item-content:eq(2)&&Text;.module-info-item-content:eq(0)&&Text",
-        "content":".module-info-introduction-content p&&Text",
+        "desc":";;;..stui-content__detail p:gt(5)&&Text;.stui-content__detail p:gt(4)&&Text",
+        "content":".detail-sketch&&Text",
         "tabs":"h3.title",
         "lists":"ul.stui-content__playlist:eq(#id)&&a"},
 
