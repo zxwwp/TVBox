f769f2209cb3dccef2a335d9c191880db76e383b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-07 00:50:17 +0000

    update js/爱看影院1.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/爱看影院1.js
+++ js/爱看影院1.js
@@ -5,7 +5,7 @@ var rule = {
 	title:'爱看影院',
 	模板:'mxpro',
 	host:'https://www.2wyy.com/',
-	url:'/vodshow/id/fyfilter.html',
+	url:'/vodshow/fyfilter-----------.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}{{fl.area}}{{fl.by}}{{fl.class}}{{fl.lang}}{{fl.letter}}/page/fypage{{fl.year}}',
 	filter: {
