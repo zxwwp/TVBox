1059517961843d7a9b0c3d75e6820132f01b7a11
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-07 02:17:23 +0000

    update js/爱看影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/爱看影院.js
+++ js/爱看影院.js
@@ -2,9 +2,7 @@ var rule={
     title:'爱看影院',
     host:'https://www.2wyy.com/',
     url:'/vodtype/fyclass-fypage.html',
-    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
-        'User-Agent':'UC_UA',
-    },
+    headers:{'User-Agent':'UC_UA',},
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'1&2&3&4',
     play_parse:true,
@@ -14,7 +12,6 @@ var rule={
     推荐:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&a.module-poster-item');tabs.forEach(function(it){var  pz=pdfh(it,'.module-poster-item-title&&Text');var ps=pdfh(it,'.module-item-douban&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.module-item-note&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
-    //二级访问前:'log(MY_URL);let jump=request(MY_URL).match(/href="(.*?)"/)[1];log(jump);MY_URL=urljoin2(MY_URL,jump)',
     二级:{
         "title":".module-info-main:eq(0) h1&&Text;.module-info-tag-link:eq(2)&&Text",
         "img":".lazyload&&data-original",
