d3cbb3a0db8855d2fb161f378871f1248b4b18be
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-12 02:58:14 +0000

    update js/速搜影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/速搜影院.js
+++ js/速搜影院.js
@@ -1,7 +1,7 @@
 var rule={
     title:'速搜影院',
     host:'https://www.sstv.pw/',
-    url:'/vodshow-fypage/page/1/?wd=',
+    url:'/vodshow-fypage/page/fypage/?wd=',
     headers:{'User-Agent':'UC_UA',},
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'1&2&3&4',
@@ -11,7 +11,7 @@ var rule={
     limit:6,
     推荐:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-        一级:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
+        一级:'.container&&.row&&.col-lg-3.col-md-6;.zmovo-team-title wapxs a&&Text;img&&src;h3.c3&&Text;a&&href',
         二级:{
         "title":".module-info-main:eq(0) h1&&Text;.module-info-tag-link:eq(2)&&Text",
         "img":".lazyload&&data-original",
