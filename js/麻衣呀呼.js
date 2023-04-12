9af674e3224e9ea4530a7b8349a40f906c7b6495
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-12 02:24:30 +0000

    update js/麻衣呀呼.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/麻衣呀呼.js
+++ js/麻衣呀呼.js
@@ -7,11 +7,11 @@ title:'麻衣呀呼',
     searchable:2,
     filterable:0,
     headers:{'User-Agent':'PC_UA', },
-    class_name:'电影&电视剧&国产剧&动漫&综艺',       //静态分类名称拼接
-    class_url:'a&b&b/area/中国大陆&c&d',    //静态分类标识拼接
+    class_name:'电影&电视剧&国产剧&香港&韩国&泰国&动漫&综艺',       //静态分类名称拼接
+    class_url:'a&b&b/area/中国大陆&b/area/香港&b/area/韩国&b/area/泰国&c&d',    //静态分类标识拼接
     推荐:'.wp.type.borsxo;.cf.listwap&&.gcol.l;*;*;*;*',
     double:true,
-    一级:'.wp.type.borsxo&&.cf.listwap&&.gcol.l;a.plusclk&&Text;.inlist.ec-jabj.eclazy&&data-original;span.packscore&&Text;a&&href',
+    一级:'.modulemod&&.cf.listwap&&.gcol.l;a.plusclk&&Text;.inlist.ec-jabj.eclazy&&data-original;span.packscore&&Text;a&&href',
 
     //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.cf.listwap .gcol.l');tabs.forEach(function(it){var  pz=pdfh(it,'a.plusclk&&Text');var ps=pdfh(it,'span.packscore&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'.inlist.ec-jabj.eclazy&&div&&data-original');var timer=pdfh(it,'.&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
      二级:{
