b661adb167171a605c8e5a132d2d82ad6871fbc2
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-12 03:36:37 +0000

    update js/茶杯狐.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/茶杯狐.js
+++ js/茶杯狐.js
@@ -7,10 +7,10 @@ var rule={
     filterable:0,
     headers:{'User-Agent':'PC_UA', },
     class_name:'电影&电视剧&国产剧&香港剧&韩国剧&泰国剧&动漫&综艺',       //静态分类名称拼接
-    class_url:'电影/全部类型/全部地区/全部年代/近期热度&电视剧/全部类型/全部地区/全部年代/近期热度&b/area/中国大陆&b/area/香港&b/area/韩国&b/area/泰国&c&d',    //静态分类标识拼接
+    class_url:'电影/全部类型/全部地区/全部年代/近期热度&电视剧/全部类型/全部地区/全部年代/近期热度&电视剧/全部类型/华语/全部年代/近期热度&b/area/香港&b/area/韩国&b/area/泰国&c&d',    //静态分类标识拼接
     推荐:'.wp.type.borsxo;.cf.listwap&&.gcol.l;*;*;*;*',
     double:true,
-    一级:'.modulemod&&.cf.listwap&&.gcol.l;a.plusclk&&Text;.inlist.ec-jabj.eclazy&&data-original;span.packscore&&Text;a&&href',
+    一级:'.jsx-600399963.card-content&&.jsx-2691002058.movie-list-body&&.jsx-2691002058.movie-list-item;.jsx-2691002058.movie-title&&Text;img&&src;.jsx-2691002058.movie-rating&&Text;a&&href',
 
     //一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.cf.listwap .gcol.l');tabs.forEach(function(it){var  pz=pdfh(it,'a.plusclk&&Text');var ps=pdfh(it,'span.packscore&&Text');var pk=pdfh(it,'.name:eq(1)&&Text');var img=pd(it,'.inlist.ec-jabj.eclazy&&div&&data-original');var timer=pdfh(it,'.&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
      二级:{
