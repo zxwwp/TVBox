aa62fc61cc98efe63bffb5c9192a9330ff4fbaf6
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-12 02:51:19 +0000

    update js/速搜影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/速搜影院.js
+++ js/速搜影院.js
@@ -1,7 +1,7 @@
 var rule={
     title:'速搜影院',
     host:'https://www.sstv.pw/',
-    url:'/vodshow-fypage',
+    url:'/vodshow-fypage/page/1/?wd=',
     headers:{'User-Agent':'UC_UA',},
     class_name:'电影&电视剧&综艺&动漫',
     class_url:'1&2&3&4',
@@ -11,8 +11,8 @@ var rule={
     limit:6,
     推荐:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.zmovo-team-content');tabs.forEach(function(it){var  pz=pdfh(it,'.module-poster-item-title&&Text');var ps=pdfh(it,'.module-item-douban&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.module-item-note&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
-    二级:{
+        一级:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
+        二级:{
         "title":".module-info-main:eq(0) h1&&Text;.module-info-tag-link:eq(2)&&Text",
         "img":".lazyload&&data-original",
         "desc":";;;.module-info-item-content:eq(2)&&Text;.module-info-item-content:eq(0)&&Text",
