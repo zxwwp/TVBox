4d2d2a9c8f64740335e106f362d70a77d81809f1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-13 03:39:10 +0000

    update js/麻衣呀呼.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/麻衣呀呼.js
+++ js/麻衣呀呼.js
@@ -5,7 +5,7 @@ var rule={
     searchable:2,//是否启用全局搜索,
     quickSearch:1,//是否启用快速搜索,
     filterable:1,//是否启用分类筛选,
-    searchUrl:'/s/id-fyclass/page/fypage/c.html?wd=**',  //搜索影片,
+    searchUrl:'/page/fypage/c.html?wd=**',  //搜索影片,
     headers:{'User-Agent':'PC_UA', },
     class_name:'电影&电视剧&国产剧&香港剧&韩国剧&泰国剧&动漫&综艺',       //静态分类名称拼接
     class_url:'a&b&b/area/中国大陆&b/area/香港&b/area/韩国&b/area/泰国&c&d',    //静态分类标识拼接
