c277d374da5b8662514672f183691d4c689aea42
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-15 00:54:58 +0000

    add js/片库网.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ js/片库网.js
@@ -0,0 +1,25 @@
+var rule={
+    title:'片库网',
+    host:'https://zqthmz.com/',
+    url:'/vodtype/fyclass--------fypage---.html',
+	searchUrl:'/vodsearch.html#wd=**;post',
+    headers:{'User-Agent':'UC_UA',},
+    class_name:'电影&电视剧&综艺&纪录片&动漫&动画片&国产剧&韩国剧&欧美剧',
+    class_url:'1&2&3&24&4&20&13&28&16',
+    play_parse:true,
+    detailUrl:'',
+    lazy:"",
+    limit:6,
+    推荐:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&a.module-poster-item');tabs.forEach(function(it){var  pz=pdfh(it,'.module-poster-item-title&&Text');var ps=pdfh(it,'.module-item-douban&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.module-item-note&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+    二级:{
+        "title":".module-info-main:eq(0) h1&&Text;.module-info-tag-link:eq(2)&&Text",
+        "img":".lazyload&&data-original",
+        "desc":";;;.module-info-item-content:eq(2)&&Text;.module-info-item-content:eq(0)&&Text",
+        "content":".module-info-introduction-content p&&Text",
+        "tabs":"#y-playList .module-tab-item",
+        "lists":".module-play-list-content:eq(#id)&&a"},
+
+   搜索:'',
+}
