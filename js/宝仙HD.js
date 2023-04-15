8923d0d60d93063f49f1d92df1e90891968b7ac5
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-15 03:26:15 +0000

    add js/宝仙HD.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

---
+++ js/宝仙HD.js
@@ -0,0 +1,21 @@
+var rule={
+    title:'宝仙HD',
+    host:'https://webdillo.com/',
+    url:'index.php/vod/type/id/fyclass-fypage.html',
+    searchUrl:'/vodsearch/**-------------.html',
+    class_name:'电影&电视剧&动漫&综艺&国产剧&港台剧&日韩剧&海外剧',       //静态分类名称拼接
+    class_url:'1&2&4&3&13&14&15&16',    //静态分类标识拼接
+    推荐:'li.col-lg-8.col-md-6.col-sm-4.col-xs-3&&.myui-vodlist__box;*;*;*;*',
+    double:true,
+    一级:'li.col-lg-8.col-md-6.col-sm-4.col-xs-3&&.myui-vodlist__box;h4.title.text-overflow&&Text;a.myui-vodlist__thumb.lazyload&&data-original;span.pic-tag.pic-tag-top&&Text;a&&href',
+   二级:{
+          title:'.myui-content__detail h1&&Text;.myui-content__detail p:eq(1)&&Text',  //片名;类型 时间
+          img:"img.lazyload&&data-original",
+          desc:';;;.myui-content__detail p:eq(3)&&Text;.myui-content__detail p:eq(4)&&Text',  //// 演员;导演
+          content:".myui-content__detail p:eq(5)&&Text", //主要信息
+          tabs:"ul.nav.nav-tabs.active li",
+          lists:'ul.myui-content__list.sort-list.clearfix:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
+          list_url:'a&&href'
+         },
+    搜索:'',
+    }
