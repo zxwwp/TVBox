5191905634b261678058c850b85363ece42a63bc
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-17 03:15:05 +0000

    update js/宝仙HD.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/宝仙HD.js
+++ js/宝仙HD.js
@@ -1,9 +1,9 @@
-muban.mxpro.二级.desc = ';;;.content_detail.content_min.fl li:eq(3)&&a&&Text;.content_detail.content_min.fl li:eq(4)&&a&&Text';
+muban.mxpro.二级.desc = '.pannel_head.clearfix h2&&Text;.content_detail.content_min.fl li:eq(0)&&Text;;;.content_detail.content_min.fl li:eq(3)&&a&&Text;.content_detail.content_min.fl li:eq(4)&&a&&Text;.content_detail.content_min.fl li:eq(5)&&Text';
 muban.mxpro.二级.tabs = 'ul.title_nav:eq(1)&&li';
 var rule = {
     title:'宝仙HD',
     模板:'mxpro',
     host:'https://webdillo.com/',
-    url:'/vodshow/fyclass--------fypage---.html',
+    url:'/index.php/vod/type/id/fyclass/page/fypage',
     class_parse: '.top_nav.clearfix&&li:gt(0):lt(6);a&&Text;a&&href;/(\\d+).html',
 }
