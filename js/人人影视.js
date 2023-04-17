937f395ef5174c273f657fc3dd5b0f410366b6f6
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-17 07:53:26 +0000

    update js/人人影视.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/人人影视.js
+++ js/人人影视.js
@@ -1,7 +1,7 @@
 muban.首图2.二级.tabs = '.stui-pannel__head h3';
 var rule = Object.assign(muban.首图2,{
     title:'人人影视',
-    host:'https://www.rttks.cc',
+    host:'https://www.rttks.com',
     url:'/rrtop/fyclass/page/fypage.html',
     searchUrl:'/rrso**/page/fypage.html',
     class_parse:'.stui-header__menu li;a&&Text;a&&href;.*/(.*?).html',
