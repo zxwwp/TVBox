a6d6c89cb24d70bb9727bb215b3c95abdc375556
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 07:34:58 +0000

    update tiyu/88看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/88看球.js
+++ tiyu/88看球.js
@@ -2,18 +2,8 @@ var rule = {
     title:'88看球',
     host:'http://www.88kanqiu.live',
     url:'/fyclass',
-    //class_name:'赛事直播✨注意时间',
-   // class_url:'/', 
     class_name:'全部&NBA&CBA&英超&西甲&意甲&德甲&法甲&中超&斯诺克',       //静态分类名称拼接
     class_url:'/&match/1/live&match/2/live&match/8/live&match/9/live&match/10/live&match/14/live&match/15/live&match/7/live&match/30/live',    //静态分类标识拼接
-    headers:{
-        'User-Agent':'PC_UA'
-    },
-    timeout:5000,
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    double:false,
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.group-game-item');tabs.forEach(function(it){var pz=pdfh(it,'.text-right&&Text');var ps=pdfh(it,'.game-type&&Text');var pk=pdfh(it,'.text-left&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.category-game-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
