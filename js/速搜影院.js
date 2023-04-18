5fdbaf5b752a71050133ebf1eb58338d71881fa0
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 06:51:34 +0000

    update js/速搜影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/速搜影院.js
+++ js/速搜影院.js
@@ -23,9 +23,9 @@ var rule={
     quickSearch:1,//是否启用快速搜索,
     class_name:'电影&剧集&综艺&动漫&记录',
     class_url:'1&2&3&4&5',
-    推荐:'',
+    推荐:'.index_vod;.vod_item;.vod_title&&Text;.lazyload&&data-src;.vod_remarks&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
-   一级:'.item&&.zmovo-team&&zmovo-full-banner-bg2;.zmovo-team-title.wapxs a&&Text;img&&src;.zmovo-team-title.wapxs h4&&Text;a&&href',
+   一级:'.tab-pane.fade.active.in.how&&row&&.item;.zmovo-team-title.wapxs a&&Text;img&&src;.zmovo-team-title.wapxs h4&&Text;a&&href',
    二级:{
           title:'h1.doc-title.mdui-text-color-white&&Text;a&&target',  //片名;类型 时间
           desc:';;;a&&Text;a&&target',  //// 演员;导演
@@ -34,5 +34,17 @@ var rule={
           lists:'.mdui-panel-item-body:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
           list_url:'a&&href'
          },
-    搜索:'',
+    搜索:'body li.vod_item;*;*;*;*',
 }
+
+    //一级:'.mdui-m-t-2&&li;.star-up-name&&Text;.lazy&&data-original;.vod_remarks&&Text;a&&href',
+    //二级访问前:'log(MY_URL);let jump=request(MY_URL).match(/href="(.*?)"/)[1];log(jump);MY_URL=urljoin2(MY_URL,jump)',
+    //二级:{
+       // "title":".video-titlewraper&&h1&&Text;.inline_item:eq(1)&&Text",
+       // "img":".lazyload&&data-original",
+       // "desc":";;;.inline_item:eq(2)&&Text;.inline_item:eq()&&Text",
+        // "content":".module-info-introduction&&Text",
+       // "content":".info_text--div&&Text",
+       // "tabs":".vod_play_list&&.mdui-panel-item-title",
+       // "lists":".mdui-panel-item-body:eq(#id)&&.mdui-btn-raised"},
+ 
