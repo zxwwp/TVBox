3b037feb886dd7a46eacb587cbad1af1aa435d71
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 07:25:39 +0000

    update tiyu/JRS低调看球.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRS低调看球.js
+++ tiyu/JRS低调看球.js
@@ -1,17 +1,13 @@
 var rule = {
     title:'JRS低调看球',
     host:'https://ddkanqiu.net/',
-    url:'/fyclass/page/fypage',
+    url:'/fyclass',
     class_name:'赛事直播✨注意时间',
     class_url:'/',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
-    timeout:5000,
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    double:false,
+    double:true,
     推荐:'*',
     一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.match-link');tabs.forEach(function(it){var pz=pdfh(it,'.left-team&&Text');var ps=pdfh(it,'.mobile-type&&Text');var pk=pdfh(it,'.right-team&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'.mobile-time&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
