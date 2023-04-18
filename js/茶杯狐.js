324cc84124c4d1dd9ec01c8cf7e70e6fbb9de6da
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 01:09:28 +0000

    update js/茶杯狐.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/茶杯狐.js
+++ js/茶杯狐.js
@@ -2,12 +2,12 @@ var rule={
     title:'茶杯狐',
     host:'https://cupfox.app/',
     url:'/filter/fyclass',
-    searchUrl:'/filter/fyclass/page/fypage',
+    searchUrl:'/filter/fyclass',
     searchable:2,
     filterable:0,
     class_name:'电影&电视剧&国产剧&欧美剧&韩国剧&泰国剧&动漫&综艺',       //静态分类名称拼接
     class_url:'电影/全部类型/全部地区/全部年代/近期热度&电视剧/全部类型/全部地区/全部年代/近期热度&电视剧/全部类型/华语/全部年代/近期热度&电视剧/全部类型/欧美/全部年代/近期热度&电视剧/全部类型/韩国/全部年代/近期热度&电视剧/全部类型/泰国/全部年代/近期热度&动漫/全部类型/全部地区/全部年代/近期热度&综艺/全部类型/全部地区/全部年代/近期热度',    //静态分类标识拼接
-    推荐:'.wp.type.borsxo;.cf.listwap&&.gcol.l;*;*;*;*',
+    推荐:'',
     double:true,
     一级:'.card.border-shadow&&.movie-list-body&&.movie-list-item;.movie-title&&Text;img&&src;.movie-rating&&Text;a&&href',
     二级:{
@@ -17,7 +17,6 @@ var rule={
           content:".tjuqing&&Text", //主要信息
           tabs:"li.this",
           lists:'.videolist.cf:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
-          //list_text:'a&&Text',
           list_url:'a&&href'
          },
     搜索:'',
