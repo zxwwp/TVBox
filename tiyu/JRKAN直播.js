21ad688df23149df845f0ce4c0bfc786365bf82b
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 07:55:57 +0000

    update tiyu/JRKAN直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/JRKAN直播.js
+++ tiyu/JRKAN直播.js
@@ -16,10 +16,10 @@ var rule = {
     二级:{
 		title:".sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text",//类型 时间
 		desc:";;;.lab_team_home&&Text;.lab_team_away&&Text",  //演员;导演
-        img:"img&&src",
+        img:".avatar img&&src",
 		content:".sub_list ul&&Text",  // 主要信息
 		tabs:"js:TABS=['【直播源】']",
-		lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_channel&&a&&strong');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});"
+		lists:"js:LISTS=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;let html=request(input);let data=pdfa(html,'.sub_playlist&&a');TABS.forEach(function(tab){let d=data.map(function(it){let name=pdfh(it,'strong&&Text');let url=pd(it,'a&&data-play');return name+'$'+url});LISTS.push(d)});"
 	    //tabs:"h3.title",
         //lists:'ul.stui-content__playlist:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
         //list_url:'a&&href'
