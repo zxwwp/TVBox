bb6289d168eb1807f79f2c2bad46630b7401e4a6
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 02:18:22 +0000

    update js/看看剧.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/看看剧.js
+++ js/看看剧.js
@@ -9,14 +9,13 @@ var rule = {
     double:true,
     一级:'.stui-vodlist__box;a&&title;a&&data-original;span.pic-text1&&Text;a&&href',
    二级:{
-          title:'.detail-info.rel.flex-auto h3&&Text;.detail-info.rel.flex-auto div:eq(1)&&Text',  //片名;类型 时间
-          img:"a.detail-pic.lazy.mask-1&&data-original",
-          desc:';;;.detail-info.rel.flex-auto div:eq(3)&&a&&Text;.detail-info.rel.flex-auto div:eq(2)&&a&&Text',  //// 演员;导演
-          content:"div#height_limit.check.text.selected.cor3&&Text", //主要信息
-          tabs:"a.swiper-slide",
-          lists:'ul.anthology-list-play.size:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
+          title:'.title wdetail h1&&Text;.stui-content__detail p:eq(0)&&Text',  //片名;类型 时间
+          img:"img&&data-original",
+          desc:';;;.stui-content__detail p:eq(6)&&a&&Text;.stui-content__detail p:eq(5)&&a&&Text',  //// 演员;导演
+          content:"span.detail-sketch&&Text", //主要信息
+          tabs:"h3.title",
+          lists:'ul.stui-content__playlist.sort-list.maxheight.clearfix:eq(#id)&&a',   //显示直播信号数量。gt是指直播信号数量从-1开始第几个，li是指直播信号数量总共有几个。
           list_url:'a&&href'
          },
     搜索:'',
-    //class_parse: '.navbar-items&&li:gt(1):lt(7);a&&title;a&&href;/(\\d+).html',
 }
