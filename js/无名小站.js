7ba4b39edce85d63162403051b52e09b12c01a81
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-18 02:43:10 +0000

    update js/无名小站.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/无名小站.js
+++ js/无名小站.js
@@ -1,7 +1,7 @@
 var rule={
     title:'无名小站',
     host:'https://blssv.com',
-    url:'/vodshow-fyclassfyfilter/',
+    url:'/vodshow-fyclass/page/fypage',
     filterable:1,//是否启用分类筛选,
     filter_url:'{{fl.area}}{{fl.by}}{{fl.class}}/page/fypage{{fl.year}}',
     filter:{
