ae3acfbd4a3f4f0d5a1816a303d72530dcbcad0a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-20 08:15:07 +0000

    update tiyu/好球直播录像回放.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/好球直播录像回放.js
+++ tiyu/好球直播录像回放.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'好球直播录像回放',
     host:'https://www.haoqiuzhibo.com/',
-    url:'/luxiang/index-2.html',     //网站的分类页面链接
+    url:'/luxiang/fyclass/index-fypage.html',     //网站的分类页面链接
     class_name:'足球录像&篮球录像&综合录像',       //静态分类名称拼接
     class_url:'zuqiu&lanqiu&tiyu',    //静态分类标识拼接
     double:false,    //是否双层列表定位,默认false
