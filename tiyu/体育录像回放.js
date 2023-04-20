23cf3991f67ed718cd3601e5aeb1521ea6a0d29a
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-20 08:21:04 +0000

    update tiyu/体育录像回放.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/体育录像回放.js
+++ tiyu/体育录像回放.js
@@ -1,5 +1,5 @@
 var rule = {
-    title:'好球直播录像回放',
+    title:'体育录像回放',
     host:'https://www.haoqiuzhibo.com/',
     url:'/luxiang/fyclass/index-fypage.html',     //网站的分类页面链接
     class_name:'足球录像&篮球录像&综合录像',       //静态分类名称拼接
