3e3e82e0fa8750a79ed58ea6f0833070e3ee78b1
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-21 07:25:46 +0000

    update js/爱看影院.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- js/爱看影院.js
+++ js/爱看影院.js
@@ -8,7 +8,7 @@ var rule = {
      url:'/vodshow/fyclassfyfilter.html',
     //url:'/vodshow/fyclass--------fypage---.html',
      filterable:1,//是否启用分类筛选,
-    filter_url:'{{fl.cateId}}-{{fl.area}}-------fypage---',
+    filter_url:'-{{fl.cateId}}-{{fl.area}}-------fypage---',
     filter:{
        "1":[
  {"key":"cateId","name":"类型","value":[
