68665cd23bd3be8de065a1db19c8fefee66e36db
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-23 01:16:54 +0000

    update tiyu/高清直播.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/高清直播.js
+++ tiyu/高清直播.js
@@ -13,7 +13,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&tr.against');tabs.forEach(function(it){var pz=pdfh(it,'strong:eq(0)&&Text');var ps=pdfh(it,'a&&Text');var pk=pdfh(it,'strong:eq(1)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.status_live&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
+    一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&tr.against:eq(#id)');tabs.forEach(function(it){var pz=pdfh(it,'strong:eq(0)&&Text');var ps=pdfh(it,'a&&Text');var pk=pdfh(it,'strong:eq(1)&&Text');var img=pd(it,'img&&src');var timer=pdfh(it,'.status_live&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz+'🆚'+pk,pic_url:img,url:url})});setResult(items);",
     二级:{
         title:".date&&Text;.against td:gt(0):lt(1)&&Text",//类型 时间
 		desc:";;;.against td:gt(3):lt(1)&&Text;.against td:gt(5):lt(6)&&Text",  //演员;导演
