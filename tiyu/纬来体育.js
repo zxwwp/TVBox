79e495c01c6dba5a23513d14b943e544548370ed
xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>
2023-04-24 01:59:36 +0000

    update tiyu/纬来体育.js.
    Signed-off-by: xl04wy22 <12010705+xl04wy22@user.noreply.gitee.com>

--- tiyu/纬来体育.js
+++ tiyu/纬来体育.js
@@ -6,7 +6,7 @@ var rule = {
     class_url:'/&zuqiu&lanqiu&lanqiu/nba&zuqiu/yingchao',    //静态分类标识拼接
     推荐:'*',
       // 类似海阔一级 列表;标题;图片;描述;链接;详情 其中最后一个参数选填
-      一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.clearfix');tabs.forEach(function(it){var  pz=pdfh(it,'div:eq(0)&&Text');var ps=pdfh(it,'a.zban_wrap p:eq(2)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'a.zban_wrap p:eq(1)&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items);",
+      一级:"js:var items=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);var tabs=pdfa(html,'body&&.clearfix');tabs.forEach(function(it){var  pz=pdfh(it,'div:eq(0)&&Text');var ps=pdfh(it,'div:eq(0)&&Text');var img=pd(it,'img&&data-original');var timer=pdfh(it,'a.zban_wrap p:eq(1)&&Text');var url=pd(it,'a&&href');items.push({desc:timer+'  '+ps,title:pz,pic_url:img,url:url})});setResult(items)",
     // 二级可以是*,表示规则无二级,直接拿一级的链接进行嗅探
      二级:{
           title:'.sig-vs-txt span&&Text;.sig-vs-txt b&&Text',  //片名;类型 时间
